49ae7f126fad642332eb2620b1df2e5485e46b16
Issao Takeuchi <issaottakeuchi@gmail.com>
2022-11-09 00:14:44 -0300

    Add some style in CSS and some warnings on application

--- avaliacao-1/src/App.jsx
+++ avaliacao-1/src/App.jsx
@@ -1,13 +1,63 @@
-import Card from "./Card";
+import './App.css'
+import Card from "./Card"
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({ name: '', color: '' })
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    if (formData.name.length < 3) {
+      let error = document.getElementsByClassName('inCaseOfError')
+      alert('Por favor, verifique os dados inseridos no formulário (mínimo de 3 caracteres)')
+      return
+    }
+
+    if (formData.name.startsWith(' ')) {
+      alert('Por favor, verifique os dados inseridos no formulário (caractere de espaço não é permitido no início)')
+      return
+    }
+
+    if (formData.color.length < 6) {
+      alert('Por favor, verifique os dados inseridos no formulário (mínimo de 6 caracteres)')
+      return
+    }
+
+    if (!(/\d/.test(formData.color))) {
+      alert('Por favor, verifique os dados inseridos no formulário (ao menos com um caractere numérico)')
+      return
+    }
+
+    const color = {}
+
+
+
+  }
 
   return (
-    <div className="App">
-      <Card />
-      <form></form>
+    <div className='App'>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className='titleOfPage'>
+          <h1>ADICIONAR NOVA COR</h1>
+        </div>
+        <div className='container_input'>
+          <div className='label_and_input'>
+            <label htmlFor='lname'>Nome</label>
+            <input value={formData.name} onChange={(event) => setFormData({ ...formData, name: event.target.value })} type='text' id='lname' />
+          </div>
+          <div className='label_and_input'>
+            <label htmlFor="lcolor">Cor</label>
+            <input value={formData.color} onChange={(event) => setFormData({ ...formData, color: event.target.value })} type='text' id='lcolor' />
+          </div>
+        </div>
+        <div className='buttons'></div>
+        <input className='addButton' type='submit' value='ADICIONAR' />
+      </form>
+      <div className='inCaseOfError'></div>
+      <div className='cards'>
+        <h1>CORES FAVORITAS</h1>
+      </div>
     </div>
   )
 }
